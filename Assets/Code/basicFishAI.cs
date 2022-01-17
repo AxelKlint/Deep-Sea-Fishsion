1232aaf4a269ac591a8d95deb52197410a6f53e8
AlexanderDang1 <97684174+AlexanderDang1@users.noreply.github.com>
2022-01-17 13:54:23 +0100

    fishAI
    some basic code for a basic fishAI. will be updated more

---
+++ Assets/Code/basicFishAI.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class basicFishAI : MonoBehaviour
+{
+    private static float fishPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fishPosition = GameObject.Find(name).transform.position.x;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(fishPosition >= 7.9f)
+        {
+            transform.position += new Vector3(-16, 0, 0) * Time.deltaTime;
+        }
+        if (fishPosition <= -7.9f)
+        {
+            transform.position += new Vector3(16, 0, 0) * Time.deltaTime;
+        }
+    }
+}
