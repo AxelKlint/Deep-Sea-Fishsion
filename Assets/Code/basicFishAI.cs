7aedd02076cebe92223ab5d8d6e05c7af51a7b8c
AlexanderDang1 <97684174+AlexanderDang1@users.noreply.github.com>
2022-01-18 12:34:06 +0100

    some more updates
    fsh

--- Assets/Code/basicFishAI.cs
+++ Assets/Code/basicFishAI.cs
@@ -5,25 +5,28 @@ using UnityEngine;
 public class basicFishAI : MonoBehaviour
 {
     private float fishPosition;
-    int movement;
+    float movement;
+    float random;
+    float timer;
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        timer = Time.deltaTime;
         fishPosition = GameObject.Find(gameObject.name).transform.position.x;
         transform.position += new Vector3(movement, 0, 0) * Time.deltaTime;
         if (fishPosition >= 7.9f)
         {
-            movement = -1;  
+            movement = -1.5f;  
         }
         if (fishPosition <= -7.9f)
         {
-            movement = 1;
+            movement = 1.5f;
         }
     }
 }
