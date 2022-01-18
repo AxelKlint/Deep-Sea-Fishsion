8d102e4140279acb6f772d5ed56060d52ce5d8fc
Edwinodeen <edwin.odeen@gmail.com>
2022-01-18 13:14:00 +0100

    score
    behöver leos kod

--- Assets/Code/Score.cs
+++ Assets/Code/Score.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Points : MonoBehaviour
+public class Score : MonoBehaviour
 {
+    public static int playscore; 
+    public Text scoretext;
+    public static float time = 1;
+    int score;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        scoretext = GetComponentInChildren<Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        scoretext.text = "score" + (Score.playscore).ToString();
     }
 }
