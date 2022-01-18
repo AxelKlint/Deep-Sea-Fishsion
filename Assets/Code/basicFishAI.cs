5965dc55a045c0c969febca02aee1c27d497f2dc
AlexanderDang1 <97684174+AlexanderDang1@users.noreply.github.com>
2022-01-18 14:38:13 +0100

    fishWeight
    makes movement better

--- Assets/Code/basicFishAI.cs
+++ Assets/Code/basicFishAI.cs
@@ -6,27 +6,61 @@ public class basicFishAI : MonoBehaviour
 {
     private float fishPosition;
     float movement;
-    float random;
+    public float yMovement;
+    float yPosition;
+    bool lastWasUp;
     float timer;
+    float speed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        yMovement = 0;
+        yPosition = GameObject.Find(name).transform.position.y;
+        speed = weightList.weight / 5;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        timer = Time.deltaTime;
+        timer += Time.deltaTime;
         fishPosition = GameObject.Find(gameObject.name).transform.position.x;
-        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime;
+        transform.position += new Vector3(movement, yMovement, 0) * Time.deltaTime;
         if (fishPosition >= 7.9f)
         {
-            movement = -1.5f;  
+            movement = -1.5f + (speed / 10);  
         }
         if (fishPosition <= -7.9f)
         {
-            movement = 1.5f;
+            movement = 1.5f - (speed / 10);
+        }
+        if (yMovement == 0 && lastWasUp == true && timer >= 0.1f)
+        {
+            yMovement += -0.5f;
+            lastWasUp = true;
+            print("up");
+            timer = 0;
+        }
+        if (yMovement == 0  && lastWasUp == false && timer >= 0.1f)
+        {
+            yMovement += 0.5f;
+            print("down");
+            timer = 0;
+        }
+        if (yMovement == 0.5f && timer >= 0.1f)
+        {
+            yMovement = 0;
+            lastWasUp = true;
+            print("mid1");
+            timer = 0;
+        }
+        if (yMovement == -0.5f && timer >= 0.1f)
+        {
+            yMovement = 0;
+            print("mid2");
+            lastWasUp = false;
+            timer = 0;
+
         }
     }
 }
