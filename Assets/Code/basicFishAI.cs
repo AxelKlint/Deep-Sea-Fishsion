0de1a614ee5066597726bf56488391a032b2493e
AlexanderDang1 <97684174+AlexanderDang1@users.noreply.github.com>
2022-01-19 15:18:32 +0100

    THE FISH WORK AGAIN
    BREAKING NEWS THEY DO

--- Assets/Code/basicFishAI.cs
+++ Assets/Code/basicFishAI.cs
@@ -5,25 +5,43 @@ using UnityEngine;
 public class basicFishAI : MonoBehaviour
 {
     private float fishPosition;
-    int movement;
+    float movement;
+    private float yMovement;
+    private float speed;
+    bool lastWasUp;
+    private float timer;
     // Start is called before the first frame update
     void Start()
     {
-
+        speed = weightList.weight;
+        fishPosition = transform.position.x;
     }
 
     // Update is called once per frame
     void Update()
     {
+        timer += Time.deltaTime;
         fishPosition = GameObject.Find(gameObject.name).transform.position.x;
-        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime;
+        transform.position += new Vector3(movement, yMovement, 0) * Time.deltaTime;
         if (fishPosition >= 7.9f)
         {
-            movement = -1;  
+            movement = -1.5f - (speed / 15);  
         }
         if (fishPosition <= -7.9f)
         {
-            movement = 1;
+            movement = 1.5f + (speed / 15);
+        }
+        if (lastWasUp == true && timer >= 0.1f)
+        {
+            yMovement = -0.5f - (speed / 20);
+            lastWasUp = false;
+            timer = 0;
+        }
+        else if (lastWasUp == false && timer >= 0.1f)
+        {
+            yMovement = 0.5f + (speed / 20);
+            lastWasUp = true;
+            timer = 0;
         }
     }
 }
