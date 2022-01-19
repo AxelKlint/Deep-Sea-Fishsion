d3e27f7dfafad9f7ddf401ce44fc8552a28d5255
Leosnubben <lwg.sandelin@gmail.com>
2022-01-19 15:36:23 +0100

    har fiskat några problem med kroken och gjort olika tags för fiskarna

--- Assets/Code/HookMovement.cs
+++ Assets/Code/HookMovement.cs
@@ -16,6 +16,8 @@ public class HookMovement : MonoBehaviour
     [SerializeField]   //Så man kan bestämma hur snabbt kroken sjunker i Unity - Leo
     float SinkSpeed;
 
+    bool hasFish = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,6 +27,12 @@ public class HookMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (transform.position.y > 0)  //Om kroken kommer över Y 0 - Leo
+        {
+            hasFish = false;
+            Destroy(transform.GetChild(0));
+            SinkSpeed = 1;
+        }
         transform.position -= new Vector3(0, SinkSpeed, 0) * Time.deltaTime; //Kroken sjunker - Leo 
 
         if (Input.GetKey(Left))
@@ -39,8 +47,17 @@ public class HookMovement : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Fish")  //Om kroken träffar en fisk - Leo
+        if (collision.gameObject.tag == "xSmall" || collision.gameObject.tag == "Small" || collision.gameObject.tag == "Medium" || collision.gameObject.tag == "Large" && hasFish == false)  //Om kroken träffar en extra liten fisk - Leo
         {
+            collision.transform.parent = gameObject.transform;  //Gör fisken till en child av kroken, den slutar alltså röra sig - Leo 
+            collision.gameObject.GetComponent<basicFishAI>().enabled = false;  //Stänger av fiskens rörelser - Leo
+            SinkSpeed *= -5;  //Kroken ändrar rikting - Leo
+            hasFish = true;
+        }
+
+        if (collision.gameObject.tag == "xLarge")  //Om kroken träffar en extra stor fisk - Leo
+        {
+            ///Koden för krob ska vara här
             collision.transform.parent = gameObject.transform;  //Gör fisken till en child av kroken, den slutar alltså röra sig - Leo 
             collision.gameObject.GetComponent<basicFishAI>().enabled = false;  //Stänger av fiskens rörelser - Leo
             SinkSpeed *= -5;  //Kroken ändrar rikting - Leo
