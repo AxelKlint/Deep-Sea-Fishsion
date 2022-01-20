09b5d25ee90c3aba25e22aa20007870e845c5904
Edwinodeen <edwin.odeen@gmail.com>
2022-01-20 13:11:17 +0100

    Score kod

---
+++ Assets/Code/Score.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class Score : MonoBehaviour
+{
+    public static int playscore;
+    public Text scoretext;
+    int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoretext = GetComponentInChildren<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoretext.text = "Score" + (score + playscore).ToString();
+    }
+}
