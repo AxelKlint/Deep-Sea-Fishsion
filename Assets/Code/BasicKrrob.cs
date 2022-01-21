02c8fc27fb03c24c2b465eb2742d72d075b519fe
Leosnubben <lwg.sandelin@gmail.com>
2022-01-21 13:22:13 +0100

    Krrob is focked

---
+++ Assets/Code/BasicKrrob.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicKrrob : MonoBehaviour
+{
+    [SerializeField]
+    float KrrobSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(KrrobSpeed, 0, 0) * Time.deltaTime;
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Wall")
+        {
+            KrrobSpeed *= -1;
+        }
+    }
+}
