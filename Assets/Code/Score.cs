79a3bff2a5eded74cd008cbf63f8ab85928b622d
Edwinodeen <edwin.odeen@gmail.com>
2022-02-09 08:54:24 +0100

    Fixat kod i krobclaw

--- Assets/Code/Score.cs
+++ Assets/Code/Score.cs
@@ -5,19 +5,19 @@ using UnityEngine.UI;
 
 public class Score : MonoBehaviour
 {
-    public static int playscore;
-    public Text scoretext;
+    public static int playscore; //
+    public Text scoretext; // Fixar scoretexten - Edwin
     int score;
 
     // Start is called before the first frame update
     void Start()
     {
-        scoretext = GetComponentInChildren<Text>();
+        scoretext = GetComponentInChildren<Text>(); // gör så att scoreexten ökar - Edwin
     }
 
     // Update is called once per frame
     void Update()
     {
-        scoretext.text = "Fishpoints " + (score + playscore).ToString();
+        scoretext.text = "Fishpoints " + (score + playscore).ToString(); // Fixar scoretexten - Edwin
     }
 }
