0666e5be5d43baa5b50b31b1da2239907146c4b2
Leosnubben <lwg.sandelin@gmail.com>
2022-02-23 09:03:39 +0100

    wertyhjk,.

--- Assets/Code/BasicKrrob.cs
+++ Assets/Code/BasicKrrob.cs
@@ -5,37 +5,36 @@ using UnityEngine;
 public class BasicKrrob : MonoBehaviour
 {
     [SerializeField]
-    float KrrobSpeed;
-    int HP = 3;
+    float KrrobSpeed;  // Så man kan ändra Krrob speed i Unity - Leo
+    int HP = 3;   // Så Krrob har 3 HP
     HookMovement Hooked;
     public bool hookable;
     // Start is called before the first frame update
     void Start()
     {
-        Hooked = GetComponent <HookMovement>();
+        Hooked = GetComponent <HookMovement>();  // När Krrob blir krokad så hämta koden för att dra upp den - Leo
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position -= new Vector3(KrrobSpeed, 0, 0) * Time.deltaTime;
-
-    }
+        transform.position -= new Vector3(KrrobSpeed, 0, 0) * Time.deltaTime;   // Krrob rör sig åt sidan - Leo
+    } 
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Wall")
+        if (collision.gameObject.tag == "Wall")   // Om kollision med "Wall" - Leo
         {
-            KrrobSpeed *= -1;
+            KrrobSpeed *= -1;   // Ändrar rikting på Krrob - Leo 
         }
-        if (collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "Player")  // Om kollision med "Player" - Leo
         {
-            HP--;
+            HP--;   // -1 HP - Leo
             print("Krabbas liv: " + HP);
         }
-        if (HP <= 0)
+        if (HP <= 0)   // OM HP är midre eller lika med 0 - Leo
         {
-            hookable = true;
+            hookable = true;   // När HP är 0 så kan man fånga krrob - Leo
         }
     }
 }
